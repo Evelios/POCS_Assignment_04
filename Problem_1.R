b9c3e31fe60f862c89c4f6a855bc08c6164d9d6c
Thomas Waters <twaters117@gmail.com>
2018-09-28 15:42:35 -0400

    Progress on Problem 1

--- Problem_1.R
+++ Problem_1.R
@@ -6,15 +6,28 @@ main = function() {
   population_count = 20000
   simulations = 100
 
-  zipf_p0_1 = richGetRicherSimulations(population_count, simulations, 0.1)
-  zipf_p0_01 = richGetRicherSimulations(population_count, simulations, 0.01)
+  zipf_p0_1   = richGetRicherSimulations(population_count, simulations, 0.1)
+  zipf_p0_01  = richGetRicherSimulations(population_count, simulations, 0.01)
   zipf_p0_001 = richGetRicherSimulations(population_count, simulations, 0.001)
 
-
-  plotZipfDistribution(zipf_p0_1, 0.1, "images/Problem1_p0_1.png")
-  plotZipfDistribution(zipf_p0_01, 0.01, "images/Problem1_p0_01.png")
+  plotZipfDistribution(zipf_p0_1,   0.1,   "images/Problem1_p0_1.png")
+  plotZipfDistribution(zipf_p0_01,  0.01,  "images/Problem1_p0_01.png")
   plotZipfDistribution(zipf_p0_001, 0.001, "images/Problem1_p0_001.png")
+
+  zipf_p0_1_filtered   = zipf_p0_1   %>% filter(log_rank < 3.2)
+  zipf_p0_01_filtered  = zipf_p0_01  %>% filter(log_rank < 2.25)
+  zipf_p0_001_filtered = zipf_p0_001 %>% filter(log_rank < 1.3)
+
+  plotZipfDistributionLm(zipf_p0_1_filtered,   0.1,   "images/Problem1_p0_1_lm.png")
+  plotZipfDistributionLm(zipf_p0_01_filtered,  0.01,  "images/Problem1_p0_01_lm.png")
+  plotZipfDistributionLm(zipf_p0_001_filtered, 0.001, "images/Problem1_p0_001_lm.png")
+
+  lm(log_freq ~ log_rank, zipf_p0_1_filtered)
+  lm(log_freq ~ log_rank, zipf_p0_01_filtered)
+  lm(log_freq ~ log_rank, zipf_p0_001_filtered)
 }
+
+# ---- Rich Get Richer Model ---------------------------------------------------
 
 richGetRicherPopulation = function(population_count, mutation_probability) {
   counter = 0
@@ -53,6 +66,8 @@ richGetRicherSimulations = function(
 
 }
 
+# ---- Plotting ----------------------------------------------------------------
+
 plotZipfDistribution = function(df, probability, file_name) {
   ggplot(df,
        aes(x=log_rank, y=log_freq)) +
@@ -62,6 +77,20 @@ plotZipfDistribution = function(df, probability, file_name) {
   geom_point()
 
   img_width = 4
+  img_height = img_width * 7 / 8
+  ggsave(file_name, device='png', units='in', width=img_width, height=img_height)
+}
+
+plotZipfDistributionLm = function(df, probability, file_name) {
+  ggplot(df,
+       aes(x=log_rank, y=log_freq)) +
+  labs(title=paste('p=', probability, 'Regression'),
+       x='Log10 Rank',
+       y='Nk, Log10 Frequency') +
+  geom_point() +
+  geom_smooth(method='lm')
+
+  img_width = 3
   img_height = img_width * 7 / 8
   ggsave(file_name, device='png', units='in', width=img_width, height=img_height)
 }
